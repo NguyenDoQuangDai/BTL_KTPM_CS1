9ed78505c0d2c435e625e4519b6c58fd6351f9fc
NguyenDoQuangDai <22028278@vnu.edu.vn>
2024-11-24 13:52:04 +0700

    Cai rate limit theo ip user 10000 request / phut bang express-rate-limit

--- KTPM-architecture-solution-main/url-shortener/server.js
+++ KTPM-architecture-solution-main/url-shortener/server.js
@@ -2,17 +2,34 @@ const express = require('express');
 const sequelize = require('./models');
 const linkController = require('./controllers/linkController');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
 // Middleware để xử lý JSON
 app.use(express.json());
 
+// Middleware Rate Limiting theo IP
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // Thời gian cửa sổ là 15 phút
+    max: 10000, // Tối đa 100 yêu cầu mỗi IP trong cửa sổ thời gian
+    standardHeaders: true, // Gửi thông tin rate limit qua các header
+    legacyHeaders: false,  // Tắt X-RateLimit-* headers cũ
+    message: 'Too many requests from this IP, please try again later.', // Thông báo khi bị giới hạn
+    keyGenerator: (req) => {
+        // Phân biệt user qua địa chỉ IP
+        return req.ip; // `req.ip` chứa địa chỉ IP của người dùng gửi request
+    },
+});
+
 // Phục vụ file tĩnh từ thư mục public (thêm caching)
 app.use(express.static(path.join(__dirname, 'public'), {
     maxAge: '3d',  // Cache tệp tĩnh trong 3 ngày
     etag: true,    // Sử dụng ETag
 }));
+
+// Áp dụng Rate Limiting cho tất cả các route
+app.use(limiter);
 
 // Định tuyến cho trang chính (index.html)
 app.get('/', (req, res) => {
